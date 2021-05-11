47599948d5a9a9f263329fcf3a2a52d1a2ebd49a
Croazt <fachry.croazt@gmail.com>
2021-05-12 01:57:14 +0700

    test

--- go.mod
+++ go.mod
@@ -1,6 +1,5 @@
 module github.com/Croazt/ProjectAkhirPemwebBE
 
-
 // +heroku goVersion go1.11
 go 1.16
 
@@ -11,7 +10,9 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/lib/pq v1.6.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
-	gorm.io/driver/mysql v1.0.6
+	gorm.io/driver/mysql v1.0.6 // indirect
+	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.9
 )
--- src/utils/dbConfig/db.go
+++ src/utils/dbConfig/db.go
@@ -3,21 +3,23 @@ package dbConfig
 import (
 	"github.com/Croazt/ProjectAkhirPemwebBE/src/utils"
 	"github.com/Croazt/ProjectAkhirPemwebBE/src/utils/dbConfig/dbModel"
-
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	host := utils.GoDotEnvVariable("DATABSE_HOST")
-	url := utils.GoDotEnvVariable("DATABSE_URL")
+	host := utils.GoDotEnvVariable("DATABASE_HOST")
+	// url := utils.GoDotEnvVariable("DATABSE_URL")
 	name := utils.GoDotEnvVariable("DATABASE_NAME")
+	user := utils.GoDotEnvVariable("DATABASE_USER")
+	password := utils.GoDotEnvVariable("DATABASE_PASSWORD")
+	port := utils.GoDotEnvVariable("DATABASE_PORT")
 
 	var err error
-	dsn := host + ":@tcp(" + url + ")/" + name + "?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := "host=" + host + " user=" + user + "  password=" + password + " dbname=" + name + " port=" + port + " sslmode=disable TimeZone=Asia/Shanghai"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database")
